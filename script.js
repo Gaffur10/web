e7e4a084aa1ecc4ae9b8ca70898c74a99d47967f
Ade Gaffur <adegaffur@gmail.com>
2023-01-13 01:12:34 +0800

    upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// const fotoProfil = document.querySelector(".header .foto-profil .fp");
+// fotoProfil.addEventListener("click", function () {
+//   fotoProfil.style.transform = "rotate(180deg)";
+// });
